4d11bca1afbfe8b08ad5ad53b1521834ff0f4ded
Futsy <Jonaks13@gmail.com>
2017-11-04 21:29:04 +0100

    A counter
    A shitty way to make a counter, but it works

---
+++ counter.py
@@ -0,0 +1,35 @@
+# Add your Python code here. E.g.
+from microbit import *
+
+counter = 9
+
+zero = Image("09990:90009:90009:90009:09990")
+one = Image("00900:09900:00900:00900:09990")
+two = Image("99990:00009:09990:90000:99999")
+three = Image("99999:00009:00090:90009:09990")
+four = Image("00990:09090:90090:99999:00090")
+five = Image("99999:90000:99990:00009:99990")
+six = Image("00090:00900:09990:90009:09990")
+seven = Image("99999:00090:00900:09000:90000")
+eight = Image("09990:90009:09990:90009:09990")
+nine = Image("09990:90009:09990:00900:09000")
+
+num = [zero, one, two, three, four, five, six, seven, eight, nine]
+
+while (True):
+    
+    display.show(num[counter])
+    
+    if button_a.is_pressed():
+        if counter == 9:
+            counter = 0
+        else:
+            counter += 1
+        
+        sleep(250)
+    
+    sleep(1000)
+    counter -= 1
+    
+    if counter < 0:
+        break
