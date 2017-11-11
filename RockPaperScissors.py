13b3729e077e20db2fbba9c8330f3f5a4e15d744
Futsy <Futseh@users.noreply.github.com>
2017-11-11 15:17:50 +0100

    rock paper scissors

---
+++ RockPaperScissors.py
@@ -0,0 +1,15 @@
+import random
+from microbit import *
+
+rock = Image("00900:09990:99999:09990:00900")
+paper = Image("99999:99999:99999:99999:99999")
+scissors = Image("99099:99099:00900:09090:90009")
+
+choices = [rock, paper, scissors]
+
+while True:
+    if accelerometer.is_gesture("shake"):
+        im = random.choice(choices)
+        display.show(im)
+    else:
+        pass
