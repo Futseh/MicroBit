2c2eda1651e184306859351ab28ce6b0efb0c6c6
Futseh <jonas.folvik@gmail.com>
2019-01-12 15:08:03 +0100

    I think it is finished now

--- plane/serialmonitor.py
+++ plane/serialmonitor.py
@@ -32,11 +32,24 @@ x = [g, 0.0, 0.0]
 y = [0.0, g, 0.0]
 z = [0.0, 0.0, g]
 
-xt = []
-yt = []
-zt = []
+ax = [0.0]
+ay = [0.0]
+az = [0.0]
+
+vx = [0.0]
+vy = [0.0]
+vz = [0.0]
+
+x = [0.0]
+y = [0.0]
+z = [0.0]
+
 r = []
 temp = []
+pitch = []
+roll = []
+time = [0.0]
+dt = 0.1
 
 for line in values:
     temp.append(line.split(' '))
@@ -44,14 +57,54 @@ for line in values:
 for i in temp:
     for j in range(len(i)):
         if i[j] == 'x:':
-            xt.append(float(i[j+1]))
+            ax.append(float(i[j+1]))
         elif i[j] == 'y:':
-            yt.append(float(i[j+1]))
+            ay.append(float(i[j+1]))
         elif i[j] == 'z:':
-            zt.append(float(i[j+1][:len(i[j+1])-3]))
+            az.append(float(i[j+1][:len(i[j+1])-3]))
 
-for i in range(len(xt)):
-    r.append([xt[i], yt[i], zt[i]])
+for i in range(len(ax)):
+    r.append([ax[i], ay[i], az[i]])
+    time.append(time[i] + dt)    
 
-for i in r:
-    print(i)
+time.pop()
+
+for i in range(len(ax)):
+    pitch.append(np.arctan2(ay[i], np.sqrt(ax[i]**2 + az[i]**2)) * 180 / np.pi)
+    roll.append(np.arctan2(-ax[i], az[i]) * 180 / np.pi)
+
+plt.plot(time, pitch, label='pitch')
+plt.plot(time, roll, label='roll')
+plt.legend()
+plt.grid()
+plt.show()
+
+plt.plot(time, ax, label='ax')
+plt.plot(time, ay, label='ay')
+plt.plot(time, az, label='az')
+plt.legend()
+plt.grid()
+plt.show()
+
+for i in range(1, len(ax)):
+    vx.append(vx[i-1] + ax[i]*dt)
+    vy.append(vy[i-1] + ay[i]*dt)
+    vz.append(vz[i-1] + az[i]*dt)
+
+    x.append(x[i-1] + vx[i]*dt)
+    y.append(y[i-1] + vy[i]*dt)
+    z.append(z[i-1] + vz[i]*dt)
+
+plt.plot(time, vx, label='vx')
+plt.plot(time, vy, label='vy')
+plt.plot(time, vz, label='vz')
+plt.legend()
+plt.grid()
+plt.show()
+
+plt.plot(time, x, label='x')
+plt.plot(time, y, label='y')
+plt.plot(time, z, label='z')
+plt.legend()
+plt.grid()
+plt.show()
